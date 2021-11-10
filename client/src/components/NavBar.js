89cb45312b4b24618f9d4a691f8465e5607131d4
Rachel Minkowitz <rhminkowitz@gmail.com>
2021-11-09 21:38:21 -0500

    navbar component including drawer

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../img/logo.png';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Link from '@material-ui/core/Link';
+import Container from '@material-ui/core/Container';
+import Hidden from '@material-ui/core/Hidden';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Divider from '@material-ui/core/Divider';
+import ChevronRightIcon from '@material-ui/icons/ChevronRightSharp';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { StylesContext } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import { Component } from 'react';
+
+const navigationLinks = [
+    { name: "Artists", href: "" },
+    { name: "Sell", href: "/SellWithUs" },
+    { name: "Cart", href: "" },
+    { name: "Login/Sign Up", href: "" },
+];
+
+const useStyles = makeStyles((theme) => ({
+    toolbar:{
+        width: "100",
+    },
+    link: {
+        marginRight: '20px',
+    },
+    menuButton: {
+        marginLeft: theme.spacing(1),
+    },
+    logo: {
+        flexGrow: 1,
+        maxWidth: 80,
+        marginRight: "auto",
+    },
+}));
+
+const Navbar = () => {
+    const styles = useStyles();
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <AppBar className={styles.appbar}>
+            <Container maxWidth="md">
+
+                <Toolbar>
+                    <img src={logo} alt="logo" className={styles.logo} />
+
+                    <Hidden xsDown>
+                        {navigationLinks.map((item) => (
+                            <Link
+                                className={styles.link}
+                                color="textPrimary"
+                                variant="button"
+                                underline="none"
+                                href={item.href}
+                                key={item.name}
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </Hidden>
+
+                    <Hidden smUp>
+                        <IconButton onClick={() => setOpen(true)}>
+                            <MenuIcon fontSize="large" color="secondary" />
+                        </IconButton>
+
+                    </Hidden>
+
+                    <IconButton
+                        color="secondary"
+                        href={"/login/signup.href"} //get ModalDialog to open when pressing the customer icon
+                    >
+                        <AccountCircleOutlinedIcon fontSize="large" />
+                    </IconButton>
+
+
+
+                </Toolbar>
+            </Container>
+
+            <SwipeableDrawer
+                anchor="right"
+                open={open}
+                onOpen={() => setOpen(true)}
+                onClose={() => setOpen(false)}
+            >
+                <div
+                    onClick={() => setOpen(false)}
+                    onKeyPress={() => setOpen(false)}
+                    role="button"
+                    tabIndex={0}>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                </div>
+
+                <Divider />
+
+                <List>
+                    {navigationLinks.map((item) => (
+                        <ListItem key={item.name}>
+                            <Link
+                                className={styles.link}
+                                color="textPrimary"
+                                variant="button"
+                                underline="none"
+                                href={item.href}
+                            >
+                                {item.name}
+                            </Link>
+
+                        </ListItem>
+                    ))}
+                </List>
+            </SwipeableDrawer>
+        </AppBar>
+    );
+};
+export default Navbar;
