c67eda7a6cffaa714a2b6755c56e0fb1545449f4
Leeran <leeran7@gmail.com>
2021-11-09 23:33:00 -0500

    started implementing user post route, need to implement setting up a cart, socials and transaction instance for each

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,14 +3,45 @@ const router = express.Router();
 const db = require('../models');
 const { User } = db;
 
+router.get("/", (req,res) => {
+    User.findAll({})
+        .then(users => res.json(users));
+})
+
 
 router.get('/user/:userid', (req,res) => { // Get User
-    res.send("GET User");
+    const { userid } = req.params;
+    User.findOne(userid)
+        .then(user => {
+            if(!user){
+                return res.sendStatus(404)
+            }
+            res.json(user);
+        })
 });
 
+router.post('/user/new', (req,res) => {
+    const { name, state, city, zipcode } = req.body;
+    User.create({
+        name, state, city, zipcode
+    })
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+})
 
 router.put('/user/:userid', (req, res) => { // Update user
-    res.send("Update User");
+    const { userid } = req.params;
+    User.findByPk(userid)
+        .then(user => {
+            if(!user){
+                return res.sendStatus(404);
+            }
+
+        })
 });
 
 router.put('/user/:userid/artworks', (req, res) => { // Get only users artworks
