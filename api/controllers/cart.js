307de7dce4981b1b959c4909fbf7aed9666235d8
Leeran <leeran7@gmail.com>
2021-11-12 16:31:35 -0500

    create user, get all users, get specific user completed

--- api/controllers/cart.js
+++ api/controllers/cart.js
@@ -4,12 +4,20 @@ const db = require('../models');
 const { Cart } = db;
 
 router.get('/cart/:userid', (req,res) => { //Open signup page
-  res.send("GET Cart");
+  return Cart.findOne({
+    where: { user_id: userid }
+  }).then(cart => {
+    return cart;
+  })
 });
 
 
 router.put('/cart/:userid', (req, res) => { //Update Cart (Delete item or Add Item)
-  res.send("POST Cart");
+  Cart.update({
+    where: { user_id: userid }
+  }).then(cart => {
+    
+  })
 });
 
 module.exports = router;
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const { User } = db;
+const { User, Cart, Social, Transaction } = db;
 
 router.get("/", (req,res) => {
     User.findAll({})
@@ -21,16 +21,43 @@ router.get('/user/:userid', (req,res) => { // Get User
 });
 
 router.post('/user/new', (req,res) => {
-    const { name, state, city, zipcode } = req.body;
+    const { name, state, city, zipcode, linkedin, instagram, twitter, facebook } = req.body;
+    // let id;
     User.create({
         name, state, city, zipcode
     })
         .then(user => {
+            Cart.create({
+                userId: user.id
+            })
+                .catch(err => {
+                    res.status(400).json(err);
+                })
+            Transaction.create({
+                userId: user.id
+            })
+                .catch(err => {
+                    res.status(400).json(err);
+                })
+            Social.create({
+                linkedin_link: linkedin,
+                facebook_link: facebook,
+                instagram_link: instagram,
+                twitter_link: twitter,
+                userId: user.id
+            })
+                .catch(err => {
+                    res.status(400).json(err);
+                })
             res.status(201).json(user);
         })
         .catch(err => {
             res.status(400).json(err);
         })
+    
+    
+
+    
 })
 
 router.put('/user/:userid', (req, res) => { // Update user
@@ -42,6 +69,7 @@ router.put('/user/:userid', (req, res) => { // Update user
             }
 
         })
+    
 });
 
 router.put('/user/:userid/artworks', (req, res) => { // Get only users artworks
