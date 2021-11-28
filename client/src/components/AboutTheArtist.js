8cd34484307de7b546112e8a9ca855c4d803d2b8
Rachel Minkowitz <rhminkowitz@gmail.com>
2021-11-28 18:54:55 -0500

    added additional social media, ready for db testing

--- client/src/components/AboutTheArtist.js
+++ client/src/components/AboutTheArtist.js
@@ -3,32 +3,40 @@ import React from "react";
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import PinterestIcon from '@material-ui/icons/Pinterest';
-
-
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 export default function AboutTheArtist(props) {
         return (
         <div>
-            <Typography>
-              <h1>  {props.name} </h1>
-              <Avatar img={props.profilePic}/>
+            <Typography component={'span'} variant={'h3'}>
+              <h1> {props.name}  <Avatar src={props.profilePic}/> </h1> 
+              
             </Typography>
             
-            <Typography>
-              <h2> {props.city} , {props.state}</h2>
+            <Typography component={'span'}>
+            <h3> {props.city}, {props.state}</h3>
               {props.bio}
             </Typography>
 
-            <IconButton src={props.insta}>
+            <IconButton href={props.insta} target="_blank">
                 <InstagramIcon fontSize="large"/>
             </IconButton>
 
-            <IconButton src={props.faceBook}>
+            <IconButton href={props.faceBook} target="_blank">
                 <FacebookIcon fontSize="large"/>
             </IconButton>
 
-            <IconButton src={props.pinterest}>
+            <IconButton href={props.pinterest} target="_blank" >
                 <PinterestIcon fontSize="large"/>
+            </IconButton>
+
+            <IconButton href={props.linkedIn} target="_blank">
+                <LinkedInIcon fontSize="large"/>
+            </IconButton>
+
+            <IconButton href={props.twitter} target="_blank">
+                <TwitterIcon fontSize="large"/>
             </IconButton>
         </div>
         );
--- client/src/pages/ArtistPage.js
+++ client/src/pages/ArtistPage.js
@@ -2,54 +2,106 @@ import React from 'react';
 import AboutTheArtist from "../components/AboutTheArtist";
 import PhotoGallery from "../components/PhotoGallery";
 
+//question: how do I get the userId for this artist????? 
 class ArtistPage extends React.Component{
     state ={
-        artistName: "",
+        artistFirstName: "",
+        artistLastName:"",
         artistCity: "",
         artistState:"",
         artistBio:"",
-        artistInsta: "",
-        artistFB:"",
-        artistEtsy:"",
-        artistPin:"",
         artistProfilePic:"",
-        //artwork: [], ??? add artwork for this artists and send as prop to photoGallery? 
+        artistInsta:'',
+        artistFB:"",
+        artistTwitter:"",
+        artistPin:"",
+        artistLinkedIn:"",
+        artwork: [],//fetches sample artwork here and passes as prop to photoGallery component
     }
     
     componentDidMount(){
-        // include artistId in fetch call
-        fetch('/api/users/artistid')
+        // include artistId in fetch call +this.state.artistName.userId
+        fetch('/api/users/')
         .then(res=> res.json())
         .then(data=> this.setState({
-            artistName: data.artistName,
+            artistFirstName: data.firstName, 
+            artistLastName: data.lastName,
             artistCity: data.city,
             artistState: data.state,
-            artistBio: data.artistBio,
-            artistInsta: data.instagram,
-            artistFB:data.faceBook,
-            artistEtsy:data.Etsy,
-            artistPin: data.Pinterest,
+            artistBio: data.bio,
             atistProfilePic: data.profilePic,
-        })
-        )
+        }))
+        .catch(err => {
+            this.setState({
+              notFound: true,
+            });
+          });
+
+        //include userId endpoint in the fetch call + this.state.artistName.userId
+        fetch('/api/socials/')
+        .then(res=> res.json())
+        .then(data=> this.setState({
+            artistInsta: data.instagram,
+            artistFB:data.facebook,
+            artistPin: data.pinterest,
+            artistLinkedIn: data.linkedin,
+            artistTwitter: data.twitter,
+        }))     
+        .catch(err => {
+            this.setState({
+              notFound: true,
+            });
+          });
+
+        fetch("/api/sampleArtwork/")
+        .then(res=> res.json())
+        .then(data=> this.setState({
+            artwork: [...this.state.artwork,
+                       data.image]
+        }))
+        .catch(err => {
+            this.setState({
+              notFound: true,
+            });
+          });
     }
     
     render(){
         return(
             <div>
             <AboutTheArtist 
-            name={this.state.artistName} 
+            firstName={this.state.artistFirstName}
+            lastName={this.state.artistLastName} 
             city={this.state.city} 
             state={this.state.state} 
-            profilePic={this.state.ArtistProfilePic} 
+            profilePic={this.state.artistProfilePic} 
             bio={this.state.artistBio}
             insta={this.state.artistInsta}
             faceBook={this.state.artistFB}
-            etsy={this.state.artistFB}
+            linkedIn={this.state.artistLinkedIn}
+            twitter={this.state.artistTwitter}
             pinterest={this.state.artistPin}
+
+
+            // name="Micheal Brown" 
+            // city="Brooklyn" 
+            // state="New York" 
+            // profilePic="https://randomuser.me/api/portraits/men/10.jpg"
+            // bio="Hi my name is Michel, I was born and bread in brooklyn NY 
+            // Hi my name is Michel, I was born and bread in brooklyn NY Hi my name is Michel, 
+            // I was born and bread in brooklyn NY
+            // Hi my name is Michel, 
+            // I was born and bread in brooklyn NY Hi my name is Michel, I
+            // was born and bread in brooklyn NY Hi my name is Michel,
+            // I was born and bread in brooklyn NY  "
+            // insta='https://www.instagram.com/noahdneiman/'
+            // faceBook={this.state.artistFB}
+            // linkedIn={this.state.artistLinkedIn}
+            // twitter={this.state.artistTwitter}
+            // pinterest={this.state.artistPin}
             />
             {/* send prop to photoGallery thats an array of all artist artwork?  */}
-            <PhotoGallery />
+            <PhotoGallery artwork={this.state.artwork}/>
             </div>
         );
     }
