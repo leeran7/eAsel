69c5be5161201ab678c72fadd025e3307e7b6498
Rachel Minkowitz <rhminkowitz@gmail.com>
2021-11-29 21:02:18 -0500

    clean up code

--- client/src/pages/ArtistList.js
+++ client/src/pages/ArtistList.js
@@ -1,35 +1,16 @@
-import React, { Component, useEffect, useState } from 'react';
-import { Grid } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-import { Container } from '@material-ui/core';
+import React from 'react';
+import {Grid , Container} from '@material-ui/core';
 import ArtistName from '../components/ArtistName';
 import Loading from '../components/Loading';
-import { Redirect } from 'react-router-dom';
 
 class ArtistList extends React.Component {
   state = {
     loading: true,
     artists: [],
-    // picture: "",
     notFound: false,
   }
 
   componentDidMount() {
-
-    // fetch('https://random-data-api.com/api/users/random_user')
-    // .then(res => res.json())
-    // .then(data => {
-    //   this.setState( prevState => ({
-    //     artists:[...prevState.artists, ...data],
-    //     loading: false,
-    //   }));z
-    // })
-    // .catch(err => {
-    //   this.setState({
-    //     notFound: true,
-    //   });
-    // });
-//}
 
     fetch('/api/users')
       .then(res => res.json())
@@ -45,7 +26,7 @@ class ArtistList extends React.Component {
         });
       });
   }
-  
+
   render() {
     if(this.state.loading) {return <Loading />};
         return(
@@ -53,7 +34,6 @@ class ArtistList extends React.Component {
             <Grid container spacing={2}>
                     <Grid item xs={12} sm={6} md={3}>
                         {/* add key  */}
-
                         {this.state.artists.map(artist=> {
                           return <ArtistName artist={artist} />
                         })}
--- client/src/pages/ArtistPage.js
+++ client/src/pages/ArtistPage.js
@@ -2,40 +2,21 @@ import React from 'react';
 import AboutTheArtist from "../components/AboutTheArtist";
 import PhotoGallery from "../components/PhotoGallery";
 
-//question: how do I get the userId for this artist????? 
 class ArtistPage extends React.Component{
     state ={
       artist : {},
       socials: {},
       artwork: []
-        // artistFirstName: "",
-        // artistLastName:"",
-        // artistCity: "",
-        // artistState:"",
-        // artistBio:"",
-        // artistProfilePic:"",
-        // artistInsta:'',
-        // artistFB:"",
-        // artistTwitter:"",
-        // artistPin:"",
-        // artistLinkedIn:"",
-        // artwork: [],//fetches sample artwork here and passes as prop to photoGallery component
     }
     
     componentDidMount(){
       const params = new URLSearchParams(window.location.search);
       const id = params.get("id");
-        // include artistId in fetch call +this.state.artistName.userId
+        // include artistId in fetch call
         fetch(`/api/users/${id}`)
         .then(res=> res.json())
         .then(data=> this.setState({
             artist : Object.assign(data)
-            // artistFirstName: data.firstName, 
-            // artistLastName: data.lastName,
-            // artistCity: data.city,
-            // artistState: data.state,
-            // artistBio: data.bio,
-            // atistProfilePic: data.profilePic,
         }))
         .catch(err => {
             this.setState({
@@ -43,26 +24,22 @@ class ArtistPage extends React.Component{
             });
           });
 
-        //include userId endpoint in the fetch call + this.state.artistName.userId
+        //include userId endpoint in the fetch call
         fetch(`/api/users/socials/${id}`)
         .then(res=> res.json())
         .then(data=> this.setState({
           socials: data
-            // artistInsta: data.instagram,
-            // artistFB:data.facebook,
-            // artistPin: data.pinterest,
-            // artistLinkedIn: data.linkedin,
-            // artistTwitter: data.twitter,
         }))     
         .catch(err => {
             this.setState({
               notFound: true,
             });
           });
+
+
         fetch(`/api/users/${id}/artworks`)
         .then(res=> res.json())
         .then(data=> {
-          // console.log(data[0].uri);
           this.setState({
             artwork: [...this.state.artwork,
                        ...data]
@@ -82,35 +59,6 @@ class ArtistPage extends React.Component{
             <AboutTheArtist 
               artist={this.state.artist}
               socials={this.state.socials}
-            // firstName={this.state.artistFirstName}
-            // lastName={this.state.artistLastName} 
-            // city={this.state.city} 
-            // state={this.state.state} 
-            // profilePic={this.state.artistProfilePic} 
-            // bio={this.state.artistBio}
-            // insta={this.state.artistInsta}
-            // facebook={this.state.artistFB}
-            // linkedin={this.state.artistLinkedIn}
-            // twitter={this.state.artistTwitter}
-            // pinterest={this.state.artistPin}
-
-
-            // name="Micheal Brown" 
-            // city="Brooklyn" 
-            // state="New York" 
-            // profilePic="https://randomuser.me/api/portraits/men/10.jpg"
-            // bio="Hi my name is Michel, I was born and bread in brooklyn NY 
-            // Hi my name is Michel, I was born and bread in brooklyn NY Hi my name is Michel, 
-            // I was born and bread in brooklyn NY
-            // Hi my name is Michel, 
-            // I was born and bread in brooklyn NY Hi my name is Michel, I
-            // was born and bread in brooklyn NY Hi my name is Michel,
-            // I was born and bread in brooklyn NY  "
-            // insta='https://www.instagram.com/noahdneiman/'
-            // faceBook={this.state.artistFB}
-            // linkedIn={this.state.artistLinkedIn}
-            // twitter={this.state.artistTwitter}
-            // pinterest={this.state.artistPin}
             />
             {/* send prop to photoGallery thats an array of all artist artwork?  */}
             <PhotoGallery artwork={this.state.artwork}/>
