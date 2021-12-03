c7a8ff20362d982e7a530da98ac0fc73ff853200
JuZNyC <junz.nyc@gmail.com>
2021-12-03 06:36:28 -0500

    Update SignUp.js

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,17 +1,5 @@
 import React from 'react';
-// import LoginButton from '../components/LoginButton';
-// import LogoutButton from '../components/LogoutButton';
 import SignUpForm from '../components/SignUpForm';
-// function SignIn() {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     // eslint-disable-next-line no-console
-//     console.log({
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     });
-//   };
 
 export default function SignUp() {
   return (
