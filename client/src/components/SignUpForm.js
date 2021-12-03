50900c0f0f88faed0bbacac2f1b87ee58416f7dc
Leeran <leeran7@gmail.com>
2021-12-03 16:45:48 -0500

    changed link href to login form

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -190,7 +190,7 @@ export default function SignUpForm() {
             </Grid>
             <Grid container>
               <Grid item>
-                <Link to="/client/src/pages/Login.js" variant="body2">
+                <Link to="/Login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
