85aabae3e8f6283f9ce91abf2b8ab647d35045db
Leeran <leeran7@gmail.com>
2021-12-08 17:23:16 -0500

    changed styling

--- client/src/components/AuthButton.js
+++ client/src/components/AuthButton.js
@@ -1,21 +1,23 @@
 import React, {useContext }from 'react'
 import { withRouter } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
-import { Button, Container, Typography } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 
 const AuthButton = withRouter(({ history }) => {
     const auth = useContext(AuthContext);
     if(!auth.isAuthenticated){
-        return <Button  href="/login">Log In</Button>
+        return <Button style={{marginLeft: "10px", borderColor: "#7e57c2"}} variant="outlined" href="/login">Log In</Button>
     }
     const logout = () => {
         auth.signout().then(() => history.push("/"));
     }
     return (
-        <div >
-            <span style={{color: "black"}}>Welcome { auth.user.firstName }</span>
-            <Button  onClick={logout}>Log Out</Button>
-        </div>
+        <Grid container>
+            <Grid item  >
+            {/* <Typography style={{fontFamily: "Roboto Condensed"}}>Welcome { auth.user.firstName }</Typography> */}
+                <Button style={{marginLeft: "10px", borderColor: "#7e57c2"}} variant="outlined" onClick={logout}> Log Out</Button>
+            </Grid>
+        </Grid>
     );
 });
 
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,41 +1,3 @@
-// import { Typography , Paper, Container} from '@material-ui/core';
-// import React, { Component } from 'react';
-// import { Redirect } from 'react-router';
-// import LoginForm from '../components/LoginForm';
-// import { AuthContext } from "../context/AuthContext";
-// import { useContext } from 'react';
-
-
-
-
-// function ProfilePage(){
-//     //so , we need to make a profile page which shows the user profile if they're logged in, 
-//     //otherwise shows the login page. 
-//     //so if the userid is empty then it displays the login page, otherwise it displays the userprofile- from the userid feature in the
-//     //db. 
-//     const auth = useContext(AuthContext);
-
-//   //  if(!auth.isAuthenticated) return<Redirect to="/login" />
-//     // if(auth.isAuthenticated){
-//      return (
-//        <div>
-         
-//          <Container>
-//          <img height="100%" width="100%" alt="profilePic" src={auth.user.profilePic}/>
-//          <Typography>Name: {auth.user.firstName} {auth.user.lastName}</Typography>
-//          <Typography>Email: {auth.user.email}</Typography>
-//          <Typography>Biography: {auth.user.bio}</Typography>
-//          <Typography>Location: {auth.user.city}, {auth.user.state} {auth.user.zipcode}</Typography>
-//          </Container>
-//        </div>
-//      );
-//     // }
-    
-
-// }
-
-// export default ProfilePage;
-
 import React from 'react';
 import LoginForm from '../components/LoginForm';
 import { AuthContext } from "../context/AuthContext";
@@ -47,6 +9,8 @@ import {
   Tabs,
   Tab,
   Typography,
+  Grid,
+  Container,
 } from "@material-ui/core";
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -99,14 +63,16 @@ function a11yProps(index) {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    //backgroundColor: theme.palette.background.paper,
+    height: "90vh",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
   },
   avatar: {
-    justifyContent: "center",
     margin: 20,
-    alignSelf: "center",
-  },
+    width: 150,
+    height: 150
+  }
 }));
 
 function ProfilePage(){
@@ -125,133 +91,149 @@ function ProfilePage(){
     
    if(auth.isAuthenticated){
      return (
-       <div className={classes.root}>
-         {/* <Stack spacing={2}> */}
-           <Avatar
-             alt="profilePic"
-             src={auth.user.profilePic}
-             className={classes.avatar}
-           />
+       <Container 
+           disableGutters
+          // xs={12} 
+          className={classes.root}>
+          <Grid
+            container
+            disableGutters
+            justifyContent="center"
+          >
+          
+            <Avatar
+              alt="profilePic"
+              src={auth.user.profilePic}
+              className={classes.avatar}
 
-           <AppBar position="static">
-             <Tabs
-               value={value}
-               onChange={handleChange}
-               aria-label="simple tabs example"
-               centered
-             >
-               <Tab label="PROFILE" {...a11yProps(0)} />
-               <Tab label="SOLD" {...a11yProps(1)} />
-               <Tab label="BOUGHT" {...a11yProps(2)} />
-             </Tabs>
-           </AppBar>
-           <TabPanel value={value} index={0}>
-             <Accordion>
-               <AccordionSummary
-                 expandIcon={<ExpandMoreIcon />}
-                 aria-controls="panel1a-content"
-                 id="panel1a-header"
-               >
-                 <Typography>Name</Typography>
-               </AccordionSummary>
-               <AccordionDetails>
-                 <Typography>
-                   {auth.user.firstName} {auth.user.lastName}
-                 </Typography>
-               </AccordionDetails>
-             </Accordion>
-             <Accordion>
-               <AccordionSummary
-                 expandIcon={<ExpandMoreIcon />}
-                 aria-controls="panel2a-content"
-                 id="panel2a-header"
-               >
-                 <Typography>Email</Typography>
-               </AccordionSummary>
-               <AccordionDetails>
-                 <Typography>{auth.user.email}</Typography>
-               </AccordionDetails>
-             </Accordion>
+            />
 
-             <Accordion>
-               <AccordionSummary
-                 expandIcon={<ExpandMoreIcon />}
-                 aria-controls="panel3a-content"
-                 id="panel3a-header"
-               >
-                 <Typography>Social Media</Typography>
-               </AccordionSummary>
-               <AccordionDetails>
-                 <List>
-                   <ListItem>
-                     <ListItemIcon>
-                       <InstagramIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText primary={auth.user.instagram} />
-                   </ListItem>
-                   <ListItem>
-                     <ListItemIcon>
-                       <FacebookIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText primary={auth.user.facebook} />
-                   </ListItem>
-                   <ListItem>
-                     <ListItemIcon>
-                       <PinterestIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText primary={auth.user.pinterest} />
-                   </ListItem>
-                   <ListItem>
-                     <ListItemIcon>
-                       <LinkedInIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText primary={auth.user.linkedin} />
-                   </ListItem>
-                   <ListItem>
-                     <ListItemIcon>
-                       <TwitterIcon fontSize="large" />
-                     </ListItemIcon>
-                     <ListItemText primary={auth.user.twitter} />
-                   </ListItem>
-                 </List>
-               </AccordionDetails>
-             </Accordion>
-             <Accordion>
-               <AccordionSummary
-                 expandIcon={<ExpandMoreIcon />}
-                 aria-controls="panel2a-content"
-                 id="panel2a-header"
-               >
-                 <Typography>Bio</Typography>
-               </AccordionSummary>
-               <AccordionDetails>
-                 <Typography>{auth.user.bio}</Typography>
-               </AccordionDetails>
-             </Accordion>
+            
+            <TabPanel value={value} index={0}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>Name</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {auth.user.firstName} {auth.user.lastName}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>Email</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{auth.user.email}</Typography>
+                </AccordionDetails>
+              </Accordion>
 
-             <Accordion>
-               <AccordionSummary
-                 expandIcon={<ExpandMoreIcon />}
-                 aria-controls="panel2a-content"
-                 id="panel2a-header"
-               >
-                 <Typography>Location</Typography>
-               </AccordionSummary>
-               <AccordionDetails>
-                 <Typography>
-                   {auth.user.city} , {auth.user.state} {auth.user.zipcode}
-                 </Typography>
-               </AccordionDetails>
-             </Accordion>
-           </TabPanel>
-           <TabPanel value={value} index={1}>
-             My Buys
-           </TabPanel>
-           <TabPanel value={value} index={2}>
-             Artworks Sold
-           </TabPanel>
-         {/* </Stack> */}
-       </div>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel3a-content"
+                  id="panel3a-header"
+                >
+                  <Typography>Social Media</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <List>
+                    <ListItem>
+                      <ListItemIcon>
+                        <InstagramIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary={auth.user.instagram} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <FacebookIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary={auth.user.facebook} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <PinterestIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary={auth.user.pinterest} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <LinkedInIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary={auth.user.linkedin} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <TwitterIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary={auth.user.twitter} />
+                    </ListItem>
+                  </List>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>Bio</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{auth.user.bio}</Typography>
+                </AccordionDetails>
+              </Accordion>
+
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>Location</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {auth.user.city} , {auth.user.state} {auth.user.zipcode}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Artworks Purchased
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              Artworks Sold
+            </TabPanel>
+            
+          </Grid>
+          <Grid>
+              <AppBar
+                className={classes.AppBar} 
+                position="sticky"
+              >
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="simple tabs example"
+                centered
+              >
+                <Tab label="PROFILE" {...a11yProps(0)} />
+                <Tab label="SOLD" {...a11yProps(1)} />
+                <Tab label="BOUGHT" {...a11yProps(2)} />
+              </Tabs>
+            </AppBar>
+          </Grid>
+       </Container>
      );}
     
 
