b5413bb59a160e0db7072753f05e69e67d841efd
Leeran <leeran7@gmail.com>
2021-12-09 01:03:47 -0500

    delete item and checkout working

--- client/src/components/CartForm.js
+++ client/src/components/CartForm.js
@@ -2,14 +2,32 @@ import { Typography, Paper, Button, Grid, Box, Container, createTheme, IconButto
 import { ThemeProvider } from '@material-ui/styles';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 import React, { useState, useEffect } from 'react';
-
+import Snackbar from '@material-ui/core/Snackbar';
+import { makeStyles } from "@material-ui/core/styles";
 const theme = createTheme();
-
+const useStyles = makeStyles((theme) => ({
+    alert: {
+      color: "black",
+      backgroundColor: "#4bb543",
+      fontSize: "16px",
+      borderRadius: "5px",
+      padding: "10px",
+      fontFamily: "Roboto Condensed",
+    }
+  }));
 export default function CartForm(props) {
     const [artworks, setArtworks] = useState([]);
-    
-    useEffect(() => {
-        
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [snackOpen , setSnackOpen] = React.useState(false);
+    const classes = useStyles();
+    const [loading, setLoading] = useState(false);
+    const handleSnackClose = (event, reason) => {
+        if (reason === "clickaway") {
+          return;
+        }
+        setSnackOpen(false);
+      };
+    const getCarts = async () => {
         fetch('/api/carts')
           .then(res => {
               if(res.ok){
@@ -17,30 +35,60 @@ export default function CartForm(props) {
               }
               console.log("FAILED");
           })
-          .then(data => {
-              getArtworks(data);
+          .then(async data => {
+             await getArtworks(data);
+             
           })
           .catch(err => {
               console.log(err);
           })
-          const getArtworks = async (data) => {
-            let i=0;
-            let urllist=[];
-            for(i; i< data.length ;i++){
-                const response = await fetch(`/api/artworks/${data[i].artworkId}`)
-                const json = await response.json();
-                urllist.push(json)
-                // console.log({urllist})
-              }
-              setArtworks(urllist);
-           }
-    }, [])
-    function deleteItem(e){
-        console.log(e.target);
     }
-//   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
-//   const taxPrice = itemsPrice * 0.08;
-//   const totalPrice = itemsPrice + taxPrice;
+    const getArtworks = async (data) => {
+        let i=0;
+        let urllist=[];
+        for(i; i< data.length ;i++){
+            const response = await fetch(`/api/artworks/${data[i].artworkId}`)
+            const json = await response.json();
+            urllist.push(json)
+          }
+          setArtworks(urllist);
+       }
+    useEffect( () => {
+        getCarts();
+          
+    }, [loading])
+
+    function deleteItem(id){
+        setLoading(true);
+        fetch(`/api/carts/${id}`, {
+            method: "DELETE"
+        })
+        .then(() => {
+            setSnackOpen(true);
+            setLoading(false)
+        })
+    }
+    function handleCheckout(){
+        setLoading(true);
+        fetch("/api/checkout", {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        })
+            .then(() => {
+                setLoading(false);
+                console.log("HERE")
+            })
+    }
+    if(artworks.length === 0){
+        return <Container style={{marginTop: "20px"}}>Cart is Empty</Container>
+    }
+    if(artworks.length > 0 && !loading){
+        // const itemsPrice = artworks.reduce((prev, next) => prev.price + next.price);
+        // const taxPrice = itemsPrice * 1.08875;
+        // setTotalPrice(taxPrice)
+    }
   return (
     <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs" spacing={3}>
@@ -51,39 +99,66 @@ export default function CartForm(props) {
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
-                }}
-            > 
-                <Grid container spacing={4}>
+                }}> 
+                <Grid>
+                    <Typography>Qty:{artworks.length}</Typography>
+                </Grid>
+                <Grid>
+                    <Typography>Total:${totalPrice}</Typography>
+                </Grid>
+                <Grid 
+                    justifyContent="center"
+                    alignItems="center"
+                    container spacing={4}>
+                        <Grid item>
+                            <Button variant="contained" onClick={handleCheckout}>Checkout</Button>
+                        </Grid>
                     {
                         artworks.map( artwork => {
                             // console.log(artwork)
                             return (
-                                <Grid item 
+                                <Grid 
+                                    
+                                    item 
                                     xs={12}
                                     
                                     key={artwork.id}>
-                                    <Paper
+                                    <Grid 
+                                        
+                                        justifyContent="center"
+                                        alignItems="center"
                                         variant="outlined"
+                                        container
+                                        direction="column"
                                         >
                                         <img width="100%" src={artwork.uri} alt="cart item"/>
-                                        <Typography>{artwork.price}</Typography>
+                                        <Typography>${artwork.price}</Typography>
                                         <Typography>{artwork.title}</Typography>
-                                        <Typography>{artwork.title}</Typography>
-                                        <Typography>{artwork.title}</Typography>
-                                        
-                                    </Paper>
-                                    <IconButton onClick={deleteItem} ><DeleteOutlineOutlinedIcon /></IconButton>
+                                        <IconButton onClick={() => deleteItem(artwork.id)}  ><DeleteOutlineOutlinedIcon /></IconButton>
+                                    </Grid>
+                                    
                                 </Grid>)
                         })
                     }
+                    
                 </Grid>
+                <Snackbar
+                    open={snackOpen}
+                    autoHideDuration={2500}
+                    onClose={handleSnackClose}
+                >
+                    <div className={classes.alert} onClose={handleSnackClose} >
+            
+                    <Typography>Successfully deleted artwork from cart 🙂</Typography>
+                    </div>
+                </Snackbar>
             </Box>
         </Container>
       </ThemeProvider>
     // <aside className="block col-1">
     //   <h2>Cart Items</h2>
     //   <div>
-    //     {cartItems.length === 0 && <div>Cart is empty</div>}
+    //     
     //     {cartItems.map((item) => (
     //       <div key={item.id} className="row">
     //         <div className="col-2">{item.name}</div>
