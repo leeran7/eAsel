ad6b31efce97ee6191ac1544112746023a082d4d
Leeran <leeran7@gmail.com>
2021-12-28 13:17:19 -0500

    added liked model for liked artworks

---
+++ api/models/liked.js
@@ -0,0 +1,20 @@
+'use strict';
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class Liked extends Model {}
+
+  Liked.init({
+  }, {
+    sequelize,
+    modelName: "liked-artworks"
+  });
+
+  Liked.associate = (models) => {
+    Liked.belongsTo(models.Artwork);
+    Liked.belongsTo(models.User);
+  };
+
+  return Liked;
+};
