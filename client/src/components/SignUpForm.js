0cc8943d5a4e7b3627e2aa3269e630ec1c98ac96
Leeran <leeran7@gmail.com>
2022-01-05 00:28:03 -0500

    added steps to signup screen, implementing confirmPassword to continue signup

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -13,8 +13,11 @@ function SignUpForm() {
   const [redirect, setRedirect] = useState(false);
   const [failed, setFailed] = useState(false);
   const [loading, setLoading] = useState(false);
-  // const [passEqual, setPassEqual ] = useState(true);
-
+  const [screen, setScreen] = useState(0);
+  const [passEqual, setPassEqual] = useState(false);
+  const [pass, setPass] = useState(null);
+  const [confirmPass, setConfirmPass] = useState(null);
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -72,6 +75,20 @@ function SignUpForm() {
           setLoading(false);
         })
   };
+  
+  const checkPassword = () => {
+    if(pass === confirmPass && pass && confirmPass){
+      setPassEqual(true)
+    } else {
+      setPassEqual(false)
+    }
+  }
+  const previousScreen = () => {
+    setScreen(screen - 1);
+  }
+  const nextScreen = () => {
+    setScreen(screen + 1);
+  }
   let err = "";
   // !passEqual && (err = "Password Mismatch..");
   // if(auth.isAuthenticated){
@@ -105,166 +122,210 @@ function SignUpForm() {
           <Grid component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2} alignItems="center"
             justifyContent="center">
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password (8 Minimum)"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  required
-                  id="city"
-                  name="city"
-                  label="City"
-                  type="text"
-                  autoComplete="address-level2"
-                />
-              </Grid>          
-              
-                         
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="state"
-                  name="state"
-                  label="State"
-                  type="text"
-                />
-              </Grid>
+              {
+                screen === 0 && <>
 
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="zipcode"
-                  name="zipcode"
-                  label="ZipCode"
-                  type="text"
-                  autoComplete="postal-code"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      type="email"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password (8 Minimum)"
+                      type="password"
+                      id="password"
+                      onChange={e => {
+                        setPass(e.currentTarget.value)
+                      }}
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="confirmPass"
+                      label="Confirm Password"
+                      type="password"
+                      id="confirmPass"
+                      onChange={e => {
+                        setConfirmPass(e.currentTarget.value)
+                        
+                      }}
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                  {
+                    !passEqual && <Typography>Complete Info to continue...</Typography>
+                  }
+                  {
+                    passEqual && <Button onClick={nextScreen}>Next</Button>
+                  }
                   
-                  fullWidth
-                  id="profilePic"
-                  name="profilePic"
-                  label="Profile Pic"
-                  type="text"
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                  
-                  fullWidth
-                  id="bio"
-                  name="bio"
-                  label="Personal Bio"
-                  type="text"
-                  multiline
-                  rows={4}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  type="url"
-                  name="pinterest"
-                  label="Pinterest"
-                  id="pinterest"
-                />
-              </Grid>
-                  
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  type="url"
-                  name="twitter"
-                  label="Twitter"
-                  id="twitter"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  type="url"
-                  name="instagram"
-                  label="Instagram"
-                  id="instagram"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  type="url"
-                  name="facebook"
-                  label="Facebook"
-                  id="facebook"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  type="url"
-                  name="linkedin"
-                  label="LinkedIn"
-                  id="linkedin"
-                />
-              </Grid>
-            <Grid item>
+                </>
+              }
+              {
+                screen === 1 && <>
+                  <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    id="city"
+                    name="city"
+                    label="City"
+                    type="text"
+                    autoComplete="address-level2"
+                  />
+                </Grid>          
+                
+                          
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="state"
+                    name="state"
+                    label="State"
+                    type="text"
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="zipcode"
+                    name="zipcode"
+                    label="ZipCode"
+                    type="text"
+                    autoComplete="postal-code"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    
+                    fullWidth
+                    id="profilePic"
+                    name="profilePic"
+                    label="Profile Pic"
+                    type="text"
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    
+                    fullWidth
+                    id="bio"
+                    name="bio"
+                    label="Personal Bio"
+                    type="text"
+                    multiline
+                    rows={4}
+                  />
+                </Grid>
+                 <Button onClick={previousScreen}>Go Back</Button>
+                 <Button onClick={nextScreen}>Next</Button>
+                </>
+              }
+              
+              {
+                screen === 2 && <>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        type="url"
+                        name="pinterest"
+                        label="Pinterest"
+                        id="pinterest"
+                      />
+                    </Grid>
+                        
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        type="url"
+                        name="twitter"
+                        label="Twitter"
+                        id="twitter"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        type="url"
+                        name="instagram"
+                        label="Instagram"
+                        id="instagram"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        type="url"
+                        name="facebook"
+                        label="Facebook"
+                        id="facebook"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        type="url"
+                        name="linkedin"
+                        label="LinkedIn"
+                        id="linkedin"
+                      />
+                    </Grid>
+                    
+                    <Grid container justifyContent='center' alignContent='center'>
+                    <Button onClick={previousScreen}>Go Back</Button>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      // sx={{ mt: 3, mb: 2 }}
+                    >Sign Up</Button>
+                  </Grid>
+                </>
+              }
+            <Grid container justifyContent='center' alignContent='center'>
               <Link style={{textDecoration: 'none', color: "black"}} to="/Login" variant="contained">
                 Already have an account? Sign in
               </Link>
             </Grid>
-            <Grid item xs={4} >
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >Sign Up</Button>
+            
             </Grid>
-          </Grid>
           </Grid>
         </Box>
       </Container>
